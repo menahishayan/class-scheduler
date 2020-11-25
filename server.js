6ef40d1ff75f0f59b7156beb5f9cecda4f56b205
Glitch (hello-express) <none>
2020-11-25 14:43:39 +0000

    🌌⛈ Checkpoint ./server.js:25521320/3346 ./package.json:25521320/2714

--- server.js
+++ server.js
@@ -5,7 +5,9 @@
 // but feel free to use whatever libraries or frameworks you'd like through `package.json`.
 const express = require("express");
 const app = express();
+var cors = require('cors')
 var request = require("request");
+const fetch = require("node-fetch");
 
 // our default array of dreams
 const dreams = [
@@ -17,27 +19,31 @@ const dreams = [
 // make all the files in 'public' available
 // https://expressjs.com/en/starter/static-files.html
 app.use(express.static("public"));
+app.use(cors())
 
 // https://expressjs.com/en/starter/basic-routing.html
 app.get("/", (request, response) => {
   var options = {
-    method: 'POST',
-    url: 'https://zoom.us/oauth/token',
+    method: "POST",
+    url: "https://zoom.us/oauth/token",
     qs: {
-        grant_type: 'authorization_code',
-        code: 'IcU8aagJf0_2vesslKITryUOBL0EQWeeQ',
-        redirect_uri: 'https://class-manage.web.app'
+      grant_type: "authorization_code",
+      code: "IcU8aagJf0_2vesslKITryUOBL0EQWeeQ",
+      redirect_uri: "https://class-manage.web.app"
     },
     headers: {
-        Authorization: 'Basic ' + Buffer.from('K09RzbDQoKa5tLEQIlw' + ':' + 'yQatemHkRICZ5z8mMpnIH8LIVBW1wI0w').toString('base64')
+      Authorization:
+        "Basic " +
+        Buffer.from(
+          "K09RzbDQoKa5tLEQIlw" + ":" + "yQatemHkRICZ5z8mMpnIH8LIVBW1wI0w"
+        ).toString("base64")
     }
-};
+  };
 
-  request(options, function(error, response, body) {
-   if (error) throw new Error(error);
-
-   response.write(body);
-  });
+  fetch(
+    "https://zoom.us/oauth/token?grant_type=authorization_code&code=IcU8aagJf0_2vesslKITryUOBL0EQWeeQ&redirect_uri=https://class-manage.web.app",
+    options
+  ).then(res => response.write(res));
 });
 
 // listen for requests :)
